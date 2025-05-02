efb9e81ffe510874dc89b060ad80960c0f6154d2
VLAD CIPRIAN CONSTANDACHE <vlad-ciprian.constandache@s.unibuc.ro>
2025-05-02 20:54:32 +0300

    link 2019

--- transactions.h
+++ transactions.h
@@ -20,7 +20,7 @@ private:
 
 public:
 
-    Transactions(Account* src, Account* dest, double amount);
+    Transactions(Account* src, Account* dest, double amount, double fee = 3);
     ~Transactions();
 
     bool execute();
